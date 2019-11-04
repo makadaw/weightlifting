7b91d3b13058730738f0b4b30702dc60476726d6
Oleksandr Kolodii <oleksandr.kolodii@booking.com>
2019-11-04 21:36:50 +0100

    Move default weights to constant update UI

--- src/Screens/Components/ExerciseRound.tsx
+++ src/Screens/Components/ExerciseRound.tsx
@@ -3,6 +3,7 @@ import { Card, CardContent, Typography, Box, Theme } from "@material-ui/core";
 import { IExcercisePercent, IPRWeigts } from "../../store/program/types";
 import makeStyles from "@material-ui/styles/makeStyles";
 import { kgFromPercentWeight } from "../../store/program/functions";
+import { defaultWeights } from "../../store/constants";
 
 const styles = makeStyles((theme: Theme) => ({
   exercis: {
@@ -15,7 +16,7 @@ const styles = makeStyles((theme: Theme) => ({
   },
   roundBox: {
     display: "flex",
-    margin: `${theme.spacing()}px`,
+    margin: `${theme.spacing()}px`
   },
   roundWeghtReps: {
     display: "flex",
@@ -24,8 +25,14 @@ const styles = makeStyles((theme: Theme) => ({
     borderRight: `1px solid ${theme.palette.divider}`,
     padding: `0 ${theme.spacing(0.5)}px`
   },
-  bottomBorder: {
-    borderBottom: `1px solid ${theme.palette.divider}`
+  roundWeights: {
+    borderBottom: `1px solid ${theme.palette.divider}`,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
+  },
+  small: {
+    fontSize: 10
   },
   roundRound: {
     margin: "auto",
@@ -41,7 +48,7 @@ type Props = {
 const ExerciseRound: React.FC<Props> = props => {
   let classes = styles(props);
   // Set default snatch and clean weights
-  let { exc, maxRounds = -1, weights = { snatch: 60, clean: 90 } } = props;
+  let { exc, maxRounds = -1, weights = defaultWeights } = props;
   let rounds = maxRounds > 0 ? exc.rounds.slice(0, maxRounds) : exc.rounds;
   return (
     <Card className={classes.exercis}>
@@ -51,15 +58,26 @@ const ExerciseRound: React.FC<Props> = props => {
           {rounds.map((r, i) => (
             <Box key={i} className={classes.roundBox}>
               <Box className={classes.roundWeghtReps}>
-                <Typography className={classes.bottomBorder}>
-                  {typeof r.weight === "string"
-                    ? r.weight
-                    : `${kgFromPercentWeight({
-                        weights,
-                        percent: r.weight as number,
-                        exc: exc.excercise[0]
-                      })} kg (${r.weight})%`}
-                </Typography>
+                <Box className={classes.roundWeights}>
+                  {typeof r.weight === "string" ? (
+                    <Typography>{r.weight}</Typography>
+                  ) : (
+                    <React.Fragment>
+                      <Typography>
+                        {kgFromPercentWeight({
+                          weights,
+                          percent: r.weight as number,
+                          exc: exc.excercise[0]
+                        })}{" "}
+                        kg
+                      </Typography>
+                      <Typography className={classes.small}>
+                        ({r.weight})%
+                      </Typography>
+                    </React.Fragment>
+                  )}
+                </Box>
+
                 <Typography>{r.reps}</Typography>
               </Box>
               <Typography className={classes.roundRound}>{r.rounds}</Typography>
--- src/store/constants.tsx
+++ src/store/constants.tsx
@@ -1,3 +1,5 @@
+import { IPRWeigts } from "./program/types";
+
 export const SET_LANGUAGE = "INCREMENT_ENTHUSIASM";
 export type SET_LANGUAGE = typeof SET_LANGUAGE;
 
@@ -6,3 +8,5 @@ export type SET_COUNTRY = typeof SET_COUNTRY;
 
 export const AUTHENTICATE = "AUTHENTICATE";
 export type AUTHENTICATE = typeof AUTHENTICATE;
+
+export const defaultWeights: IPRWeigts = { snatch: 62, clean: 90 };
